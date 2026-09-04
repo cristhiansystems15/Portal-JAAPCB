4df2f4d1884d87b6a76f2f22a9b1f506e9ae3499
cristhiansystems15 <cristhianosorio503@gmail.com>
2026-09-04 16:22:25 -0600

    Update API_URL to new endpoint

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL='https://script.google.com/macros/s/AKfycbxKQI_b67PBpFgAxY-0-dAaoVtdQ3E1SUZxZeXEO9QPepJbTzZXEgKiChdQNZQDmgHLfQ/exec';
+const API_URL='https://script.google.com/macros/s/AKfycbzy9wONtVS11Klbo1466zhScgViQa-nKFsapzaNIxSt0e6afxSRLHn-ba5MDX7t8HIcOg/exec';
 const $=id=>document.getElementById(id);
 const money=n=>new Intl.NumberFormat('es-HN',{style:'currency',currency:'HNL',minimumFractionDigits:2}).format(Number(n)||0);
 
