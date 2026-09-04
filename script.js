3055ba3cf02ae71f19743c42182bda735e28b434
cristhiansystems15 <cristhianosorio503@gmail.com>
2026-09-04 14:41:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL='https://script.google.com/macros/s/AKfycbxKQI_b67PBpFgAxY-0-dAaoVtdQ3E1SUZxZeXEO9QPepJbTzZXEgKiChdQNZQDmgHLfQ/exec';
+const API_URL='https://script.google.com/macros/s/AKfycbx_ZTNSDOfUAUxkmGvGFbkQUn03PogP1s_unxSgkS347BT0NzdGDL4kOMEJSBa-bbLHtQ/exec';
 const $=id=>document.getElementById(id);
 const money=n=>new Intl.NumberFormat('es-HN',{style:'currency',currency:'HNL',minimumFractionDigits:2}).format(Number(n)||0);
 
